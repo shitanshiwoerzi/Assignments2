549ae9ba485fba132965f6349b0874d4d4bfc42e
Ziqi Zhou <20091593@mail.wit.ie>
2020-12-29 14:47:11 +0000

    Inital Version

---
+++ movies-api/api/users/index.js
@@ -0,0 +1,95 @@
+import express from 'express';
+import User from './userModel';
+import jwt from 'jsonwebtoken';
+import movieModel from '../movies/movieModel';
+
+const router = express.Router();
+
+// Get all users
+router.get('/', (req, res, next) => {
+    User.find().then(users =>  res.status(200).json(users))
+    .catch(next);
+});
+
+// Register OR authenticate a user
+router.post('/', async (req, res, next) => {
+  if (!req.body.username || !req.body.password) {
+    res.status(401).json({
+      success: false,
+      msg: 'Please pass username and password.',
+    });
+  }
+  if (req.query.action === 'register') {
+    const pattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/;
+    if(pattern.test(req.body.password)){
+    await User.create(req.body).catch(next);
+    res.status(201).json({
+      code: 201,
+      msg: 'Successful created new user.',
+    });
+    }else{
+      res.status(403).send("Password is invalid");
+    }
+  } else {
+    const user = await User.findByUserName(req.body.username).catch(next);
+      if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
+      user.comparePassword(req.body.password, (err, isMatch) => {
+        if (isMatch && !err) {
+          // if user is found and password is right create a token
+          const token = jwt.sign(user.username, process.env.SECRET);
+          // return the information including token as JSON
+          res.status(200).json({
+            success: true,
+            token: 'BEARER ' + token,
+          });
+        } else {
+          res.status(401).json({
+            code: 401,
+            msg: 'Authentication failed. Wrong password.'
+          });
+        }
+      });
+    }
+});
+
+// Update a user
+router.put('/:id', (req, res, next) => {
+    if(req.body._id) delete req.body._id;
+    User.update({
+        _id:req.params.id,
+    },req.body,{
+        upsert: false,
+    })
+    .then(user => res.json(200,user))
+    .catch(next);
+});
+
+//Add a favourite. No Error Handling Yet. Can add duplicates too!
+router.post('/:userName/favourites', async (req, res, next) => {
+  try{
+  const newFavourite = req.body.id;
+  const userName = req.params.userName;
+  const movie = await movieModel.findByMovieDBId(newFavourite);
+  const user = await User.findByUserName(userName);
+  if(user.favourites.indexOf(movie._id) === -1){
+    await user.favourites.push(movie._id);
+  }else{
+    res.status(403).send("The movie already exists");
+  }
+  await user.save(); 
+  res.status(201).json(user);
+  }catch(error){
+    next(error);
+  }
+});
+
+// Get Movie Favourites
+router.get('/:userName/favourites', (req, res, next) => {
+  const userName = req.params.userName;
+  User.findByUserName(userName).populate('favourites').then(
+    user => res.status(201).json(user.favourites)
+  ).catch(next);
+});
+
+
+export default router;
---
+++ movies-api/index.js
@@ -0,0 +1,49 @@
+import dotenv, { load } from 'dotenv';
+import express from 'express';
+import moviesRouter from './api/movies';
+import bodyParser from 'body-parser';
+import './db';
+import {loadUsers, loadMovies} from './seedData';
+import usersRouter from './api/users';
+import genresRouter from './api/genres'
+import passport from './authenticate';
+import session from 'express-session';
+
+dotenv.config();
+
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
+};
+
+if (process.env.SEED_DB) {
+  loadUsers();
+  loadMovies();
+}
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(passport.initialize());
+
+app.use(session({
+  secret: 'ilikecake',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(express.static('public'));
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/genres', genresRouter);
+app.use(errHandler);
+
+
+app.listen(port, () => {
+  console.info(`Server running at ${port}`);
+});
